17e6922ccb80cbcb821cdaa4d583a55395a7635a
Nate <77979206+nobodynate@users.noreply.github.com>
2025-03-21 13:50:57 -0500

    asdf

--- cloudgoat/scenarios/aws/beanstalk_secrets/terraform/output.tf
+++ cloudgoat/scenarios/aws/beanstalk_secrets/terraform/output.tf
@@ -1,7 +1,7 @@
 output "initial_low_priv_credentials" {
   description = "Initial low-privileged credentials provided to the scenario"
   value = <<EOF
-Access Key: ${awsssss_iam_access_key.low_priv_key.id}
+Access Key: ${aws_iam_access_key.low_priv_key.id}
 Secret Key: ${aws_iam_access_key.low_priv_key.secret}
 EOF
   sensitive = true
